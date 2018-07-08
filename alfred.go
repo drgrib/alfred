fe78b0589f48e4dd1602d9d848945852ff0094ad
Chris Redford <credford@gmail.com>
2018-07-08 12:11:04 -0700

    package doc

--- alfred.go
+++ alfred.go
@@ -1,6 +1,6 @@
-package alfred
-
 // Package alfred is a lean but comprehensive implementation of the Alfred Script Filter JSON Format to get Alfred workflows off the ground quickly with blazingly fast script filters in Go that can be seamlessly developed inside or outside Alfred (https://www.alfredapp.com/)
+
+package alfred
 
 import (
 	"encoding/json"
